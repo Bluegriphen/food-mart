0181e74a9a7b39b0f8ffe151a417dd4dc2f58503
Aarti-solanki <aartibasolanki25@gmail.com>
2026-01-27 18:56:20 -0800

    admin commits added

--- admin/src/pages/add/Add.jsx
+++ admin/src/pages/add/Add.jsx
@@ -117,7 +117,7 @@ const Add = ({ url }) => {
               value={data.price}
               type="number"
               name="price"
-              placeholder="$20"
+              placeholder="₹20"
               required
             />
           </div>
