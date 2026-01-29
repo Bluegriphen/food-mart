6523184d49b8193d6c7869eb941946151af3d507
Priyankaba solanki <223sbece54041@vsitr.ac.in>
2026-01-28 18:27:23 -0800

    intial commits

--- server/routes/chatbot.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { Message } from "../controllers/chatbot.message.js";
-
-const router = express.Router();
-
-// POST route for sending a message to the chatbot
-router.post("/Message", Message);
-
-export default router;
